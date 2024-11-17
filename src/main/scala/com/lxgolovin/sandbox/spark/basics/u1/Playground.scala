09f61360f120563160777f7d843ececdf6290fd5
lxgolovin <lxgolovin@gmail.com>
2024-11-17 20:12:51 +0300

    adding fmt

--- src/main/scala/com/lxgolovin/sandbox/spark/basics/u1/Playground.scala
+++ src/main/scala/com/lxgolovin/sandbox/spark/basics/u1/Playground.scala
@@ -2,21 +2,17 @@ package com.lxgolovin.sandbox.spark.basics.u1
 
 import org.apache.spark.sql.SparkSession
 
-object layground extends App {
+object Playground extends App {
 
-
-  val spark = SparkSession.builder()
+  val spark = SparkSession
+    .builder()
     .appName("Playground")
     .master("local")
     .getOrCreate()
 
-  val courses = Seq(
-    ("Scala", 22),
-    ("Spark", 30)
-  )
+  val courses = Seq(("Scala", 22), ("Spark", 30))
 
   import spark.implicits._
-
   val coursesDF = courses.toDF("title", "duration (h)")
   coursesDF.show()
   coursesDF.printSchema()
